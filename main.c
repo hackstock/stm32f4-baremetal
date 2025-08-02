a2cfdf60dee0bf8cfe5d50bbae38deeb71866c42
Edward Pie <king@Mac.fritz.box>
2025-08-03 01:07:12 +0200

    copies .data and .bss from FLASH to SRAM in Reset_Handler

--- main.c
+++ main.c
@@ -1,7 +1,24 @@
+#include <stdint.h>
+
+void turnOnLED3(void);
 
 int main(void){
-    
+    turnOnLED3();
     while(1);
 
     return 0;
+}
+
+void turnOnLED3(void){
+    uint32_t *pRCC_AHB1ENR = (uint32_t*)0x40023830;
+    uint32_t *pGPIOD_MODER = (uint32_t*)0x40020c00;
+    uint32_t *pGPIOD_ODR = (uint32_t*)0x40020c14;
+
+    // enables clock access to GPIOD
+    *pRCC_AHB1ENR |= (1UL << 3);
+    // set PD13 as output
+    *pGPIOD_MODER &= !(0b11 << 26);
+    *pGPIOD_MODER |= (0b01 << 26);
+    // turn on LED
+    *pGPIOD_ODR |= (1 << 13);
 }
--- startup.c
+++ startup.c
@@ -6,6 +6,11 @@ void Default_Handler(void);
 void NMI_Handler(void) __attribute__ ((weak, alias("Default_Handler")));
 
 extern unsigned int _estack;
+extern unsigned int _etext;
+extern unsigned int _sdata;
+extern unsigned int _edata;
+extern unsigned int _sbss;
+extern unsigned int _ebss;
 
 uint32_t vector_table[] __attribute__ ((section(".isr_vector_tbl"))) = {
     (uint32_t)&_estack,
@@ -14,6 +19,23 @@ uint32_t vector_table[] __attribute__ ((section(".isr_vector_tbl"))) = {
 };
 
 void Reset_Handler(void){
+    uint32_t data_mem_size = (uint32_t)&_edata - (uint32_t)&_sdata;
+    uint32_t bss_mem_size = (uint32_t)&_ebss - (uint32_t)&_sbss;
+
+    // copy data from FLASH to SRAM
+    uint32_t *p_src = (uint32_t *)&_etext;
+    uint32_t *p_dest = (uint32_t *)&_sdata;
+    for(uint32_t i =0; i < data_mem_size; i++){
+        *p_dest++ = *p_src++;
+    }
+
+    // initialize bss to zeros in SRAM
+    p_dest = (uint32_t *)&_sbss;
+    for(uint32_t i =0; i < bss_mem_size; i++){
+        *p_dest++ = 0;
+    }
+
+    // invoke main function
     main();
 }
 
